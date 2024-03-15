e8f2740a7188390b61802917e994a05a652f7979
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-03-15 02:47:45 +0200

    fix main method

--- Lab-1/Trapezoid-App/Trapezoid-Java/src/main/java/mouse/project/Main.java
+++ Lab-1/Trapezoid-App/Trapezoid-Java/src/main/java/mouse/project/Main.java
@@ -1,7 +1,7 @@
-package org.example;
+package mouse.project;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, world");
     }
 }
