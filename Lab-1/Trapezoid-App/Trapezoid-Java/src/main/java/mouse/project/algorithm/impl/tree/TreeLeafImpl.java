516766354ca12ca991b9b4fdd6e0900e10192401
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-03-22 01:56:18 +0200

    add empty implementations

--- Lab-1/Trapezoid-App/Trapezoid-Java/src/main/java/mouse/project/algorithm/impl/tree/TreeLeafImpl.java
+++ Lab-1/Trapezoid-App/Trapezoid-Java/src/main/java/mouse/project/algorithm/impl/tree/TreeLeafImpl.java
@@ -1,4 +1,13 @@
 package mouse.project.algorithm.impl.tree;
 
 public class TreeLeafImpl implements TreeElement {
+    @Override
+    public void setLeft(TreeElement element) {
+
+    }
+
+    @Override
+    public void setRight(TreeElement element) {
+
+    }
 }
