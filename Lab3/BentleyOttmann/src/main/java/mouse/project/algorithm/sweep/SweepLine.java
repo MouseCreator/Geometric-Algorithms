331f40ca4834d29e30d71394152030112f5c7c22
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-04-26 10:07:33 +0300

    fix angle comparator

--- Lab3/BentleyOttmann/src/main/java/mouse/project/algorithm/sweep/SweepLine.java
+++ Lab3/BentleyOttmann/src/main/java/mouse/project/algorithm/sweep/SweepLine.java
@@ -30,6 +30,21 @@ public class SweepLine {
         return e1.position().x() - e2.position().x();
     };
 
+    private static final Comparator<TSegment> angleComparator = (s1, s2) -> {
+        Vector2 v1 = s1.direction();
+        Vector2 v2 = s2.direction();
+        Vector2 left = Vector2.of(1, 0);
+        double diff = left.cos(v1) - left.cos(v2);
+        double tolerance = 0.0001;
+        if (Math.abs(diff) < tolerance) {
+            return 0;
+        }
+        if (diff < -tolerance) {
+            return -1;
+        }
+        return 1;
+    };
+
     private interface Event {
         Position position();
     }
@@ -40,35 +55,7 @@ public class SweepLine {
     private record EndEvent(Position position, TSegment segment) implements Event {
     }
 
-    private static class IntersectionEvent implements Event {
-        private final Position position;
-        private final RBTree<TSegment> orderedSegments;
-        private static final Comparator<TSegment> comparator = (s1, s2) -> {
-            Vector2 v1 = s1.direction();
-            Vector2 v2 = s2.direction();
-            Vector2 left = Vector2.of(1, 0);
-            double diff = left.cos(v1) - left.cos(v2);
-            double tolerance = 0.0001;
-            if (Math.abs(diff) < tolerance) {
-                return 0;
-            }
-            if (diff < -tolerance) {
-                return -1;
-            }
-            return 1;
-        };
-        public IntersectionEvent(Position position) {
-            this.position = position;
-            orderedSegments = new RBTreeImpl<>(comparator);
-        }
-        public void add(TSegment segment) {
-            this.orderedSegments.insert(segment);
-        }
-
-        @Override
-        public Position position() {
-            return position;
-        }
+    private record IntersectionEvent(Position position, TSegment s1, TSegment s2) implements Event {
     }
 
     public void scan(TSegmentSet segmentSet) {
