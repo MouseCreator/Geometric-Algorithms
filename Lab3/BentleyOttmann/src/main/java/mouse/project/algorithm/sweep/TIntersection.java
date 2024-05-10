5107a756ab6a835e902b9e92fce4cb101d3fac4c
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-05-10 12:07:00 +0300

    fix double-intersections

--- Lab3/BentleyOttmann/src/main/java/mouse/project/algorithm/sweep/TIntersection.java
+++ Lab3/BentleyOttmann/src/main/java/mouse/project/algorithm/sweep/TIntersection.java
@@ -3,7 +3,25 @@ package mouse.project.algorithm.sweep;
 
 import mouse.project.math.FPosition;
 
+import java.util.Objects;
+
+
 public record TIntersection(TSegment segment1, TSegment segment2, FPosition intersection) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TIntersection that = (TIntersection) o;
+        return Objects.equals(segment1, that.segment1) && Objects.equals(segment2, that.segment2)
+                || Objects.equals(segment2, that.segment1) && Objects.equals(segment1, that.segment2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(segment1, segment2);
+    }
+
     public String print() {
         return segment1 + " intersects " + segment2 + " at " + intersection;
     }
